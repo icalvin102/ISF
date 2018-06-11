e4397358c37f1c9e90a226f12790a4c594007f62
Immanuel Calvin Herchenbach <calvin@icalvin.de>
2018-06-11 03:02:41 +0200

    added RadialSpectrogram shader

--- GradientDisplace.fs
+++ GradientDisplace.fs
@@ -17,9 +17,13 @@
     {
       "NAME" : "amount",
       "TYPE" : "float",
-      "MAX" : 1,
+      "MAX" : 5,
       "DEFAULT" : 0.01,
       "MIN" : 0
+    },
+    {
+      "NAME" : "smooth",
+      "TYPE" : "bool"
     }
   ],
   "PASSES" : [
@@ -33,16 +37,24 @@
 
 void main()	{
 	vec2 uv = isf_FragNormCoord.xy;
-	vec2 e = vec2(.0001, 0.0);
-	vec2 g = vec2(IMG_NORM_PIXEL(displace, uv + e.xy).r -
-				  			IMG_NORM_PIXEL(displace, uv - e.xy).r,
-				  			IMG_NORM_PIXEL(displace, uv + e.yx).r -
-				  			IMG_NORM_PIXEL(displace, uv - e.yx).r
-	);
-	g /= e.x;
+	vec3 e = vec3(1.0 / IMG_SIZE(displace).xy, 0.0);
 
-	g *= IMG_NORM_PIXEL(displace, uv).r;
+	vec3 gx = IMG_NORM_PIXEL(displace, uv + e.xz).rgb - IMG_NORM_PIXEL(displace, uv - e.xz).rgb;
+	vec3 gy = IMG_NORM_PIXEL(displace, uv + e.yz).rgb - IMG_NORM_PIXEL(displace, uv - e.yz).rgb;
+	//gx /= 2.0*e.x;
+	//gy /= 2.0*e.y;
+	if(smooth){
+		vec3 strength = IMG_NORM_PIXEL(displace, uv).rgb;
+		gx *= strength;
+		gy *= strength;
+	}
 
-	gl_FragColor = IMG_NORM_PIXEL(inputImage, uv + g);
+	gx *= amount;
+	gy *= amount;
+	vec3 color = vec3(0.0);
+	color.r = IMG_NORM_PIXEL(inputImage, uv + vec2(gx.r, gy.r)).r;
+	color.g = IMG_NORM_PIXEL(inputImage, uv + vec2(gx.g, gy.g)).g;
+	color.b = IMG_NORM_PIXEL(inputImage, uv + vec2(gx.b, gy.b)).b;
+	gl_FragColor = vec4(color, 1.0);
 
 }
